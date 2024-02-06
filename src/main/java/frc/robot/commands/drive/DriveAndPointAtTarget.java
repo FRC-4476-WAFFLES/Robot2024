e784edae7808bece74368d2cc61371e951f1af5e
Brennan Bibic <brennan.bibic@gmail.com>
2024-02-06 14:08:15 -0500

    ctre logging added

--- src/main/java/frc/robot/commands/drive/DriveAndPointAtTarget.java
+++ src/main/java/frc/robot/commands/drive/DriveAndPointAtTarget.java
@@ -7,6 +7,7 @@ package frc.robot.commands.drive;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.DriveConstants;
+import frc.robot.commands.shooter.SpinUp;
 
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
@@ -33,11 +34,14 @@ public class DriveAndPointAtTarget extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    SpinUp spinUp = new SpinUp();
+    spinUp.execute();
+
     driveSubsystem.setControl(
       new SwerveRequest.FieldCentricFacingAngle()
         .withDeadband(DriveConstants.maxSpeed * 0.1) // Add a 10% deadband to translation
@@ -51,7 +55,10 @@ public class DriveAndPointAtTarget extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    SpinUp spinUp = new SpinUp();
+    spinUp.end(interrupted);
+  }
 
   // Returns true when the command should end.
   @Override
