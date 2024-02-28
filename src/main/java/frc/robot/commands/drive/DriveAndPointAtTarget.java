4c1fa57fbfc15bc7040b0b5008d429fb00094949
Benjamin4476 <bendenotterversteeg@gmail.com>
2024-02-27 20:25:24 -0500

    Update DriveAndPointAtTarget.java

--- src/main/java/frc/robot/commands/drive/DriveAndPointAtTarget.java
+++ src/main/java/frc/robot/commands/drive/DriveAndPointAtTarget.java
@@ -7,13 +7,13 @@ package frc.robot.commands.drive;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.DriveConstants;
-import frc.robot.commands.shooter.SpinUp;
 
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.SteerRequestType;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.FieldCentricFacingAngle;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
 import static frc.robot.RobotContainer.*;
@@ -34,31 +34,28 @@ public class DriveAndPointAtTarget extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    SpinUp spinUp = new SpinUp();
-    spinUp.execute();
-
-    driveSubsystem.setControl(
-      new SwerveRequest.FieldCentricFacingAngle()
-        .withDeadband(DriveConstants.maxSpeed * 0.1) // Add a 10% deadband to translation
+    FieldCentricFacingAngle request = new SwerveRequest.FieldCentricFacingAngle()
+        .withDeadband(DriveConstants.maxSpeed * 0.03) // Add a 3% deadband to translation
         .withDriveRequestType(DriveRequestType.Velocity)
         .withSteerRequestType(SteerRequestType.MotionMagic)
         .withVelocityX(xVelocitySupplier.getAsDouble())
         .withVelocityY(yVelocitySupplier.getAsDouble())
-        .withTargetDirection(thetaSupplier.get())
-    );
+        .withTargetDirection(thetaSupplier.get());
+    
+    request.HeadingController.setP(.1);
+    request.HeadingController.setD(0);
+
+    driveSubsystem.setControl(request);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    SpinUp spinUp = new SpinUp();
-    spinUp.end(interrupted);
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
