8b23f8f228c03d7b6628ca7f6952ea8e96ea9024
Brennan Bibic <brennan.bibic@gmail.com>
2024-09-11 09:49:47 -0400

    Back to somewhat the original

--- src/main/java/frc/robot/commands/ActivateLightColour.java
+++ src/main/java/frc/robot/commands/ActivateLightColour.java
@@ -34,78 +34,55 @@ public class ActivateLightColour extends Command {
     final double matchTimer = Timer.getMatchTime();
     LimelightHelpers.setLEDMode_ForceOff(LIMELIGHT_NAME);
     
-    // If certain conditions are fulfilled, the robot's lights change colours
-    if (isReadyToShoot()) {
+    // If certain conditions are fufilled, the robot's lights change colours
+    if(shooterSubsystem.isGoodSpeed() && anglerSubsystem.isGoodShooterAngle() && 
+    elevatorSubsystem.isGoodElevatorPosition() && shooterSubsystem.isShooterRunning() && 
+    shooterSubsystem.isTryingToShoot() && driveSubsystem.notMoving()){
       // Ready to shoot  
-      setLightPattern(LightColours.GREEN, LightColours.GREEN, false);
-    } else if (shooterSubsystem.isNote()) {
+      lightSubsystem.setLEDRangeGroup(LedRange.RIGHT_SIDE_FULL, LightColours.GREEN,LightColours.GREEN,false);
+      lightSubsystem.setLEDRangeGroup(LedRange.LEFT_SIDE_FULL, LightColours.GREEN,LightColours.GREEN,false);
+
+    } 
+    else if (shooterSubsystem.isNote()) {
       // Note in Shooter
-      setLightPattern(LightColours.PINK, LightColours.PINK, false);
-    } else if (isIntakeRunning()) {
+      lightSubsystem.setLEDRangeGroup(LedRange.RIGHT_SIDE_FULL, LightColours.PINK,LightColours.PINK,false);
+      lightSubsystem.setLEDRangeGroup(LedRange.LEFT_SIDE_FULL, LightColours.PINK,LightColours.PINK,false);
+    }
+    else if (intakeSubsystem.isRunningIn() && anglerSubsystem.isGoodShooterAngle() && elevatorSubsystem.isGoodElevatorPosition()){
       // Intake running
-      setLightPattern(LightColours.YELLOW, LightColours.BLACK, true);
-    } else if (intakeSubsystem.isRunningOut()) {
-      setLightPattern(LightColours.RED, LightColours.BLACK, true);
-    } else if (isLimelightDetecting()) {
-      setLimelightDetectedPattern();
-    } else {
-      setAlliancePattern();
+      lightSubsystem.setLEDRangeGroup(LedRange.RIGHT_SIDE_FULL, LightColours.YELLOW,LightColours.BLACK,true);
+      lightSubsystem.setLEDRangeGroup(LedRange.LEFT_SIDE_FULL, LightColours.YELLOW,LightColours.BLACK,true);
+    }
+    else if (intakeSubsystem.isRunningOut()){
+      lightSubsystem.setLEDRangeGroup(LedRange.RIGHT_SIDE_FULL, LightColours.RED,LightColours.BLACK,true);
+      lightSubsystem.setLEDRangeGroup(LedRange.LEFT_SIDE_FULL, LightColours.RED,LightColours.BLACK,true);
+    }
+    else if (LimelightHelpers.getTV(LIMELIGHT_NAME) && LimelightHelpers.getTA(LIMELIGHT_NAME) > 1.0){
+      lightSubsystem.setBlinkTime(0.08);
+      lightSubsystem.setLEDRangeGroup(LedRange.MIDDLE_FULL, LightColours.BLUE,LightColours.ORANGE,true);
+      LimelightHelpers.setLEDMode_ForceBlink(LIMELIGHT_NAME);
+    }
+    else {
+      
+      lightSubsystem.setBlinkTime(0.1);
+      var alliance = DriverStation.getAlliance();
+      if (alliance.get() == DriverStation.Alliance.Blue) {
+        lightSubsystem.setLEDRangeGroup(LedRange.FULL_RANGE, LightColours.BLUE,LightColours.YELLOW,false);
+      }
+      else {
+        lightSubsystem.setLEDRangeGroup(LedRange.FULL_RANGE, LightColours.RED,LightColours.YELLOW,false);
+      }
+
     }
         
-    if (isEndgameWarning(matchTimer)) {
+    if (20 < matchTimer && matchTimer < 25){
       // Endgame warning
-      lightSubsystem.setLEDRangeGroup(LedRange.FULL_RANGE, LightColours.RED, LightColours.WHITE, true);
-    } else if (isMatchEnding(matchTimer)) {
-      lightSubsystem.setLEDRangeGroup(LedRange.FULL_RANGE, LightColours.PURPLE, LightColours.ORANGE, true);
-    }  
-  }
-
-  private boolean isReadyToShoot() {
-    return shooterSubsystem.isGoodSpeed() && 
-           anglerSubsystem.isGoodShooterAngle() && 
-           elevatorSubsystem.isGoodElevatorPosition() && 
-           shooterSubsystem.isShooterRunning() && 
-           shooterSubsystem.isTryingToShoot() && 
-           driveSubsystem.notMoving();
-  }
-
-  private boolean isIntakeRunning() {
-    return intakeSubsystem.isRunningIn() && 
-           anglerSubsystem.isGoodShooterAngle() && 
-           elevatorSubsystem.isGoodElevatorPosition();
-  }
-
-  private boolean isLimelightDetecting() {
-    return LimelightHelpers.getTV(LIMELIGHT_NAME) && LimelightHelpers.getTA(LIMELIGHT_NAME) > 1.0;
-  }
-
-  private boolean isEndgameWarning(double matchTimer) {
-    return 20 < matchTimer && matchTimer < 25;
-  }
-
-  private boolean isMatchEnding(double matchTimer) {
-    return 0 < matchTimer && matchTimer < 5;
-  }
-
-  private void setLightPattern(LightColours primary, LightColours secondary, boolean blink) {
-    lightSubsystem.setLEDRangeGroup(LedRange.RIGHT_SIDE_FULL, primary, secondary, blink);
-    lightSubsystem.setLEDRangeGroup(LedRange.LEFT_SIDE_FULL, primary, secondary, blink);
-  }
-
-  private void setLimelightDetectedPattern() {
-    lightSubsystem.setBlinkTime(0.08);
-    lightSubsystem.setLEDRangeGroup(LedRange.MIDDLE_FULL, LightColours.BLUE, LightColours.ORANGE, true);
-    LimelightHelpers.setLEDMode_ForceBlink(LIMELIGHT_NAME);
-  }
-
-  private void setAlliancePattern() {
-    lightSubsystem.setBlinkTime(0.1);
-    var alliance = DriverStation.getAlliance();
-    if (alliance.get() == DriverStation.Alliance.Blue) {
-      lightSubsystem.setLEDRangeGroup(LedRange.FULL_RANGE, LightColours.BLUE, LightColours.YELLOW, false);
-    } else {
-      lightSubsystem.setLEDRangeGroup(LedRange.FULL_RANGE, LightColours.RED, LightColours.YELLOW, false);
+      lightSubsystem.setLEDRangeGroup(LedRange.FULL_RANGE, LightColours.RED,LightColours.WHITE,true);
     }
+    else if(0 < matchTimer && matchTimer < 5){
+      lightSubsystem.setLEDRangeGroup(LedRange.FULL_RANGE, LightColours.PURPLE,LightColours.ORANGE,true);
+    }  
+
   }
 
   // Called once the command ends or is interrupted.
